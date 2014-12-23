df5a7a11f73e924fd818e0d4f130a192ae6bcc02
Ian Macalinao <me@ian.pw>
2014-12-23 16:30:52 -0600

    Add gulp serve

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var connect = require('gulp-connect');
 var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
 var minifyHTML = require('gulp-minify-html');
@@ -9,4 +10,10 @@ gulp.task('js', function() {
     .pipe(concat('app.js'))
     .pipe(uglify())
     .pipe(gulp.dest('assets/'));
+});
+
+gulp.task('serve', function() {
+  connect.server({
+    livereload: true
+  });
 });
